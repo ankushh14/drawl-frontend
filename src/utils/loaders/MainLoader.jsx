1358e81f54e949fe232e3e0ddd20016dc758e24d
ankushh14 <ankushshenoy97@gmail.com>
2024-01-20 19:09:20 +0530

    style : MainLoader z-index correction

--- src/utils/loaders/MainLoader.jsx
+++ src/utils/loaders/MainLoader.jsx
@@ -7,7 +7,7 @@ export default function MainLoader() {
   return (
      status === STATUS.PENDING 
      && 
-    <div className="back-div fixed top-0 left-0 right-0 bottom-0 bg-white flex justify-center items-center">
+    <div className="back-div absolute top-0 left-0 right-0 bottom-0 z-40 bg-white flex justify-center items-center">
       <div className="load-div flex justify-center space-x-1 items-center">
         <h1 className="text-[20px] font-senibold text-black xl:text-[40px]">NexusMeetHub</h1>
         <div className="svg-div w-[45px] h-[40px] animate-pulse delay-75 xl:w-[55px] xl:h-[50px]">
