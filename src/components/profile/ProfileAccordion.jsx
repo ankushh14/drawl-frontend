fb0fd3143e1ce98eada6b5e08617075ec041de69
ankushh14 <ankushshenoy97@gmail.com>
2024-01-25 14:02:46 +0530

    feat : Workspace Member list added

--- src/components/profile/ProfileAccordion.jsx
+++ src/components/profile/ProfileAccordion.jsx
@@ -1,64 +1,84 @@
 import PropTypes from 'prop-types'
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { FaEye, FaEyeSlash, FaRegCopy } from "react-icons/fa";
 import { showToastMessage } from '../../utils/toasts/showToast';
 
 
 export default function ProfileAccordion({ workspaceName, workspacePassword, workspaceMembers }) {
     const [passwordVisibility, setPasswordVisibility] = useState(true)
-    const [edit,setEdit] = useState(false)
+    const [edit, setEdit] = useState(false)
+    const [isPopulated, setIsPopulated] = useState(false)
 
-    const copyPassword = ()=>{
+    const copyPassword = () => {
         navigator.clipboard.writeText(workspacePassword)
-        showToastMessage("copied to clipboard","success")
+        showToastMessage("copied to clipboard", "success")
     }
+
+    const checkPopulated = useCallback(() => {
+        if (workspaceMembers.length > 0) {
+            return setIsPopulated(true)
+        } else {
+            return
+        }
+    }, [workspaceMembers, setIsPopulated])
+
+    useEffect(() => {
+        checkPopulated()
+    }, [checkPopulated])
+
     return (
-        <>
-            <div className="main-title w-full flex flex-col lg:flex-row p-3 rounded-md border shadow shadow-slate-400 mt-2 mb-1 first:mt-0 last:mb-0">
-                <h1 className="w-full md:w-full pb-2 lg:pb-0">
-                    {workspaceName}
-                </h1>
-                <div className="primary-buttons-div w-full md:w-fit flex">
-                    <button className={`border text-white bg-slate-400 mx-1 rounded-md px-4 py-2 text-xs lg:text-sm flex justify-center items-center active:scale-95 transition-all duration-500`} onClick={()=>setEdit((prev)=>!prev)}>{edit?"Done":"Edit"}</button>
-                    <button className={`border text-white bg-red-400 mx-1 rounded-md px-4 py-2 text-xs lg:text-sm flex justify-center items-center active:scale-95 transition-all duration-500`}>Delete</button>
+            <div className="w-full flex flex-col p-3 rounded-md border shadow shadow-slate-400 mt-2 mb-1 first:mt-0 last:mb-0">
+                <div className="w-full flex flex-col lg:flex-row pb-2">
+                    <h1 className="w-full md:w-full pb-2 lg:pb-0">
+                        {workspaceName}
+                    </h1>
+                    <div className="primary-buttons-div w-full md:w-fit flex">
+                        <button className={`border text-white bg-slate-400 mx-1 rounded-md px-4 py-2 text-xs lg:text-sm flex justify-center items-center active:scale-95 transition-all duration-500`} onClick={() => setEdit((prev) => !prev)}>{edit ? "Done" : "Edit"}</button>
+                        <button className={`border text-white bg-red-400 mx-1 rounded-md px-4 py-2 text-xs lg:text-sm flex justify-center items-center active:scale-95 transition-all duration-500`}>Delete</button>
+                    </div>
                 </div>
-            </div>
-            <div className={`w-full flex flex-col text-xs bg-slate-200 rounded-md transition-height duration-500 ${edit?"h-[250px]":"h-0"} overflow-hidden`}>
+                <div className={`w-full flex flex-col text-xs border-slate-300  rounded-md transition-height transition-border duration-500 ${edit ? "h-[185px] border" : "h-0 border-none"} overflow-hidden`}>
                 <div className={`password-div flex flex-col p-2 w-full lg:w-[50%]`}>
-                    <h1 className={`text-[0.55rem] w-full`}>Password</h1>
+                    <h1 className={`text-[0.65rem] w-full`}>Password</h1>
                     <h1 className={`w-full flex justify-between`}>
                         {
                             workspacePassword.length > 0 ?
-                             <>
-                                <input type={passwordVisibility ? "password" : "text"} disabled value={workspacePassword} />
-                                <div className={`w-fit ${workspacePassword.length > 0 ? "flex" : "hidden"}`}>
-                                    <div className="w-fit  cursor-pointer mr-2" onClick={()=>setPasswordVisibility((prev)=>!prev)}>
-                                        {
-                                            passwordVisibility?
-                                                <FaEyeSlash className='text-slate-500' />
-                                                :
-                                                <FaEye className='text-slate-500'/>
-                                        }
-                                        
+                                <>
+                                    <input type={passwordVisibility ? "password" : "text"} disabled value={workspacePassword} />
+                                    <div className={`w-fit ${workspacePassword.length > 0 ? "flex" : "hidden"}`}>
+                                        <div className="w-fit  cursor-pointer mr-2" onClick={() => setPasswordVisibility((prev) => !prev)}>
+                                            {
+                                                passwordVisibility ?
+                                                    <FaEyeSlash className='text-slate-500' />
+                                                    :
+                                                    <FaEye className='text-slate-500' />
+                                            }
+
+                                        </div>
+                                        <div className='w-fit  cursor-pointer' onClick={copyPassword}>
+                                            <FaRegCopy className='text-slate-500' />
+                                        </div>
                                     </div>
-                                    <div className='w-fit  cursor-pointer' onClick={copyPassword}>
-                                        <FaRegCopy className='text-slate-500'/>
-                                    </div>
-                                </div>
-                            </>:
-                            <span>None</span>
-                    }
+                                </> :
+                                <span>None</span>
+                        }
                     </h1>
                 </div>
-                <div className={`w-full flex flex-col`}>
-                    {workspaceMembers.map((member,index)=>{
-                        return <div key={index} className='w-full lg:w-[50%] border'>
-                            <h1>{member}</h1>
-                        </div>
-                    })}
+                <div className={`w-full flex flex-col px-2`}>
+                    <h1 className='text-[0.65rem]'>Workspace members (Excluding you)</h1>
+                    {
+                        !isPopulated ?
+                            <h1>None</h1>
+                            :
+                            workspaceMembers.map((member, index) => {
+                                return <div key={index} className='w-full lg:w-[50%]'>
+                                    <h1>{member}</h1>
+                                </div>
+                            })
+                    }
                 </div>
             </div>
-        </>
+            </div>
     )
 }
 
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -5,11 +5,13 @@ import { useCallback, useEffect, useState } from "react";
 import { useWorkspacesUpdate } from "../hooks/useWorkspaceCount";
 import ProfileAccordion from "../components/profile/ProfileAccordion";
 import { showToastMessage } from "../utils/toasts/showToast";
+import useTheme from "../hooks/useTheme";
 
 export default function ProfilePage() {
     const { user,token } = useAuth()
     const {updateWorkspaceCount} = useWorkspacesUpdate()
     const [accordionData,setAccordionData] = useState(null)
+    const {darkMode} = useTheme()
 
     const getUserWorkspaces = useCallback(async()=>{
         let requestBody = {
@@ -29,12 +31,12 @@ export default function ProfilePage() {
     },[getUserWorkspaces,updateWorkspaceCount])
 
     return (
-        <div className="main-profile-div w-full flex flex-col lg:flex-row p-5">
-            <div className="main-profile w-full lg:w-[50%] flex flex-col justify-center items-center p-5 h-[400px]">
+        <div className={`main-profile-div w-full flex flex-col lg:flex-row p-5 ${darkMode?"text-white bg-black":"bg-white text-black"} min-h-screen`}>
+            <div className="main-profile w-full lg:w-[50%] flex flex-col justify-center items-center p-5 max-h-[400px]">
                 <div className="profile-img-div w-[40%] rounded-full border-8 relative">
                     <img src={user.profile} alt={user.email + "profile image"} className="w-full rounded-full h-full" />
                     <div className="rounded-full absolute w-7 h-7 cursor-pointer flex justify-center items-center right-0 bg-white">
-                        <FaPen size={14} />
+                        <FaPen size={14} className="text-black" />
                     </div>
                 </div>
                 <div className="profile-content-div w-full flex justify-center flex-col items-center pt-4">
@@ -47,6 +49,7 @@ export default function ProfilePage() {
                 </div>
             </div>
             <div className="workspaces-div w-full lg:w-[50%] flex flex-col">
+                <h1 className="font-bold text-xl mb-2">Your workspaces</h1>
                 {
                     accordionData?.map((accordion,index)=>{
                         return <ProfileAccordion key={index} workspaceName={accordion.name} workspaceMembers={accordion.members} workspacePassword={accordion.password}/>
