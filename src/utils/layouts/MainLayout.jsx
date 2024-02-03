94a136ea9f772fe047c64a38941894b02bd0c7d5
ankushh14 <ankushshenoy97@gmail.com>
2024-02-04 01:12:16 +0530

    style : Extra div removed

--- src/utils/layouts/MainLayout.jsx
+++ src/utils/layouts/MainLayout.jsx
@@ -4,9 +4,7 @@ import { Outlet } from "react-router-dom"
 export default function MainLayout() {
   return (
     <div className="w-full flex flex-col">
-        <div className="navbar-div w-full">
-            <Navbar/>
-        </div>
+        <Navbar/>
         <div className="main-div w-full">
             <Outlet/>
         </div>
