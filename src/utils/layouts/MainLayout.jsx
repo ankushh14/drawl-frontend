c28b469fc0bdf9d3043b4818f0550e89e9d3fcae
ankushh14 <ankushshenoy97@gmail.com>
2024-02-05 21:51:59 +0530

    style: bg-update

--- src/utils/layouts/MainLayout.jsx
+++ src/utils/layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../../components/navbar/Navbar"
 import { Outlet } from "react-router-dom"
+import useTheme from "../../hooks/useTheme"
 
 export default function MainLayout() {
+  const { darkMode } = useTheme()
   return (
-    <div className="w-full flex flex-col">
+    <div className={`main-layout w-full flex flex-col ${darkMode?"bg-[#212529]":"bg-white"}`}>
         <Navbar/>
         <div className="main-div w-full">
             <Outlet/>
