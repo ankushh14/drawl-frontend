a20cf070e63c154149007586a9694daddf5aa4fb
ankushh14 <ankushshenoy97@gmail.com>
2024-02-08 21:54:05 +0530

    feat : Dark mode added to all pages

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,15 +1,26 @@
-import {useState} from 'react'
-import Register from '../components/auth/Register'
-import Login from '../components/auth/Login'
+import { useState } from "react";
+import Register from "../components/auth/Register";
+import Login from "../components/auth/Login";
+import useTheme from "../hooks/useTheme";
 
 export default function AuthPage() {
-  const [newUser,setNewUser] = useState(false)  
+  const [newUser, setNewUser] = useState(false);
+  const { darkMode } = useTheme();
   return (
-    <div className="main-div w-full h-screen flex justify-center items-center">
-      {
-        newUser?<Register setNewUser={setNewUser}/>:<Login setNewUser={setNewUser}/>
-      }
+    <div
+      className={`main-div w-full h-screen flex justify-center items-center
+    ${
+      darkMode
+        ? "bg-[#212529]  text-white border-white"
+        : "bg-white text-black border-[#d3d3d3]"
+    }
+    `}
+    >
+      {newUser ? (
+        <Register setNewUser={setNewUser} />
+      ) : (
+        <Login setNewUser={setNewUser} />
+      )}
     </div>
-  )
+  );
 }
-
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,29 +1,49 @@
-import { FaArrowRight } from "react-icons/fa"
-import { useNavigate } from "react-router-dom"
+import { FaArrowRight } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import useTheme from "../hooks/useTheme";
 
 export default function LandingPage() {
   const navigate = useNavigate();
+  const { darkMode } = useTheme()
   return (
-    <div className="main-div w-full h-screen flex flex-col justify-center items-center p-6">
+    <div className={`main-div w-full h-screen flex flex-col justify-center items-center p-6
+    ${
+      darkMode
+        ? "bg-[#212529]  text-white border-white"
+        : "bg-white text-black border-[#d3d3d3]"
+    }
+    `}>
       <div className="main-heading w-full flex justify-center items-center py-3">
-      <h1 className="text-4xl md:text-7xl font-bold text-center">
-        Nexus Meet hub
-      </h1>
+        <h1 className="text-4xl md:text-7xl font-bold text-center">
+          Nexus Meet hub
+        </h1>
       </div>
       <div className="secondary-heading w-full flex justify-center items-center py-3 mb-8">
-      <h1 className="text-2xl md:text-5xl font-semibold text-center">
-        A Collaborative Networking Experience
-      </h1>
+        <h1 className="text-2xl md:text-5xl font-semibold text-center">
+          A Collaborative Networking Experience
+        </h1>
       </div>
       <div className="side-heading w-full flex justify-center items-center py-3 md:px-16">
-      <p className="text-center text-xs md:text-base">
-        Welcome to NexusMeethub, the innovative web application designed to redefine your online interactions. NexusMeethub combines seamless chatting capabilities with a unique and creative twist - introducing the Blankboard feature for collaborative drawing and scribbling. Whether you are planning a project, discussing ideas, or just looking to have fun with friends, NexusMeethub is the ultimate destination for interactive and dynamic conversations.
-      </p>
+        <p className="text-center text-xs md:text-base">
+          Welcome to NexusMeethub, the innovative web application designed to
+          redefine your online interactions. NexusMeethub combines seamless
+          chatting capabilities with a unique and creative twist - introducing
+          the Blankboard feature for collaborative drawing and scribbling.
+          Whether you are planning a project, discussing ideas, or just looking
+          to have fun with friends, NexusMeethub is the ultimate destination for
+          interactive and dynamic conversations.
+        </p>
       </div>
       <div className="btn-dashboard">
-        <button type="button" className="bg-black text-white rounded-md px-8 py-3 flex justify-center items-center hover:text-[#FBFFFF] active:scale-95 transition-all duration-500" onClick={()=>navigate("/dashboard")}>Dashboard<FaArrowRight className="ml-1"/></button>
+        <button
+          type="button"
+          className="bg-black text-white rounded-md px-8 py-3 flex justify-center items-center hover:text-[#FBFFFF] active:scale-95 transition-all duration-500"
+          onClick={() => navigate("/dashboard")}
+        >
+          Dashboard
+          <FaArrowRight className="ml-1" />
+        </button>
       </div>
     </div>
-  )
+  );
 }
-
