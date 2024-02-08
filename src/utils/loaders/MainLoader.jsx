7f3c84df28103c8bec354b62aadb6b81eb3f0cb0
ankushh14 <ankushshenoy97@gmail.com>
2024-02-09 00:45:35 +0530

    style: Main Loader font updated and svg removed

--- src/utils/loaders/MainLoader.jsx
+++ src/utils/loaders/MainLoader.jsx
@@ -1,22 +1,22 @@
-import earth from "../../assets/earth.svg"
-import { useAuth } from "../../hooks/useAuth"
-import useTheme from "../../hooks/useTheme"
-import STATUS from "../status"
+import { useAuth } from "../../hooks/useAuth";
+import useTheme from "../../hooks/useTheme";
+import STATUS from "../status";
 
 export default function MainLoader() {
-  const {status} = useAuth()
-  const {darkMode} = useTheme()
+  const { status } = useAuth();
+  const { darkMode } = useTheme();
   return (
-     status === STATUS.PENDING 
-     && 
-    <div className={`back-div fixed top-0 left-0 right-0 bottom-0 z-40 flex justify-center items-center ${darkMode ? "bg-[#212529] text-white" : "bg-white text-black"}`}>
-      <div className="load-div flex justify-center space-x-1 items-center">
-        <h1 className="text-[20px] font-senibold text-inherit xl:text-[40px]">NexusMeetHub</h1>
-        <div className="svg-div w-[45px] h-[40px] animate-pulse delay-75 xl:w-[55px] xl:h-[50px]">
-          <img src={earth} alt="earth" className="w-full h-full" />
+    status === STATUS.PENDING && (
+      <div
+        className={`back-div fixed top-0 left-0 right-0 bottom-0 z-40 flex justify-center items-center 
+        ${darkMode ? "bg-[#212529] text-white" : "bg-white text-black"}`}
+      >
+        <div className="load-div flex justify-center space-x-1 items-center">
+          <h1 className="text-[20px] font-semibold text-inherit xl:text-[40px] animate-pulse delay-75 font-Aclonica">
+            NexusMeetHub
+          </h1>
         </div>
       </div>
-    </div>
-  )
+    )
+  );
 }
-
