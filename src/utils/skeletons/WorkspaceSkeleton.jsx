bc936b313c0e73d17a129bfd6ff84c10a85062c9
ankushh14 <ankushshenoy97@gmail.com>
2024-02-12 22:46:22 +0530

    style : removed extra padding

--- src/utils/skeletons/WorkspaceSkeleton.jsx
+++ src/utils/skeletons/WorkspaceSkeleton.jsx
@@ -11,7 +11,7 @@ export default function WorkspaceSkeleton() {
   ])
   return (
     <div
-      className={`cards-cont flex flex-wrap overflow-y-scroll justify-center h-[80%] lg:h-auto lg:overflow-hidden w-full  lg:w-[70%]  p-4  ${
+      className={`cards-cont flex flex-wrap overflow-y-scroll justify-center h-full lg:h-auto lg:overflow-hidden w-full ${
         darkMode
           ? "bg-[#212529] text-white  shadow-white"
           : "bg-white text-black shadow-slate-500"
