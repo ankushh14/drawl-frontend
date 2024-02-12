90574ded6bf4bd6f1aaf587d9bcb5f8a419a5746
ankushh14 <ankushshenoy97@gmail.com>
2024-02-12 22:46:06 +0530

    chore : Moved workspaces fetch from Home page to WorkspacesInHome

--- src/components/home/WorkspacesInHome.jsx
+++ src/components/home/WorkspacesInHome.jsx
@@ -1,20 +1,64 @@
-import PropTypes from "prop-types"
-import useTheme from "../../hooks/useTheme"
-import WorkspaceCard from "../workspace/WorkspaceCard"
+import PropTypes from "prop-types";
+import useTheme from "../../hooks/useTheme";
+import WorkspaceCard from "../workspace/WorkspaceCard";
+import { getWorkspaces } from "../../api/workspace";
+import { showToastMessage } from "../../utils/toasts/showToast";
+import { useCallback, useEffect, useState } from "react";
+import { useWorkspacesUpdate } from "../../hooks/useWorkspaceCount";
+import { useAuth } from "../../hooks/useAuth";
+import WorkspaceSkeleton from "../../utils/skeletons/WorkspaceSkeleton";
 
-export default function WorkspacesInHome({ workspaces }) {
-    const { darkMode } = useTheme()
-    return (
-        <div className={`cards-cont flex flex-wrap overflow-y-scroll justify-center h-[80%] lg:h-auto lg:overflow-hidden w-full  lg:w-[70%]  p-4  ${darkMode ? "bg-[#212529] text-white  shadow-white" : "bg-white text-black shadow-slate-500"}`}>
-            {
-                workspaces.map((item, index) => {
-                    return <WorkspaceCard workspace={item} key={index}/>
-                })
-            }
-        </div>
-    )
+export default function WorkspacesInHome({ setNoWorkspace }) {
+  const { darkMode } = useTheme();
+  const { updateWorkspaceCount } = useWorkspacesUpdate();
+  const [skeleton, setSkeleton] = useState(false);
+  const { user, token } = useAuth();
+  const [workspaces, setWorkspaces] = useState([]);
+
+  const getworkspaces = useCallback(async () => {
+    setSkeleton(true);
+    const requestBody = {
+      owner: user.email,
+    };
+    const response = await getWorkspaces(requestBody, token);
+    if (response.valid) {
+      setSkeleton(false);
+      if (response.data.length === 0) {
+        return setNoWorkspace(true);
+      }
+      return setWorkspaces(response.data);
+    } else {
+      setSkeleton(false);
+      return showToastMessage(
+        response.message + ",Please refresh the page",
+        response.info
+      );
+    }
+  }, [user, token, setNoWorkspace]);
+
+  useEffect(() => {
+    getworkspaces();
+  }, [getworkspaces, updateWorkspaceCount]);
+
+  return (
+    <div
+      className={`cards-cont flex flex-wrap overflow-y-scroll justify-center h-[80%] lg:h-auto lg:overflow-hidden w-full  lg:w-[70%]  p-4  ${
+        darkMode
+          ? "bg-[#212529] text-white  shadow-white"
+          : "bg-white text-black shadow-slate-500"
+      }`}
+    >
+      {skeleton ? (
+        <WorkspaceSkeleton />
+      ) : (
+        workspaces.map((item, index) => {
+          return <WorkspaceCard workspace={item} key={index} />;
+        })
+      )}
+    </div>
+  );
 }
 
 WorkspacesInHome.propTypes = {
-    workspaces: PropTypes.array.isRequired
-}
+  setNoWorkspace: PropTypes.func.isRequired,
+};
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,50 +1,17 @@
 import AsideBar from "../components/home/AsideBar";
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 import NoWorkSpaceComponent from "../components/home/NoWorkSpaceComponent";
 import WorkSpaceModal from "../components/workspace/WorkSpaceModal";
 import useTheme from "../hooks/useTheme";
-import { getWorkspaces } from "../api/workspace";
-import { useAuth } from "../hooks/useAuth";
-import { showToastMessage } from "../utils/toasts/showToast";
 import WorkspacesInHome from "../components/home/WorkspacesInHome";
-import { useWorkspacesUpdate } from "../hooks/useWorkspaceCount";
 import JoinWorkspaceModal from "../components/workspace/JoinWorkspaceModal";
-import WorkspaceSkeleton from "../utils/skeletons/WorkspaceSkeleton";
+
 
 export default function HomePage() {
   const { darkMode } = useTheme();
-  const { user, token } = useAuth();
-  const [workspaces, setWorkspaces] = useState([]);
   const [createModal, setCreateModal] = useState(false);
   const [joinModal, setJoinModal] = useState(false);
-  const { updateWorkspaceCount } = useWorkspacesUpdate();
-  const [skeleton, setSkeleton] = useState(false);
   const [noWorkspace, setNoWorkspace] = useState(false);
-
-  const getworkspaces = useCallback(async () => {
-    setSkeleton(true);
-    const requestBody = {
-      owner: user.email,
-    };
-    const response = await getWorkspaces(requestBody, token);
-    if (response.valid) {
-      setSkeleton(false);
-      if (response.data.length === 0) {
-        return setNoWorkspace(true);
-      }
-      return setWorkspaces(response.data);
-    } else {
-      setSkeleton(false);
-      return showToastMessage(
-        response.message + ",Please refresh the page",
-        response.info
-      );
-    }
-  }, [user, token]);
-
-  useEffect(() => {
-    getworkspaces();
-  }, [getworkspaces,updateWorkspaceCount]);
 
   return (
     <div
@@ -54,11 +21,7 @@ export default function HomePage() {
     >
       {!noWorkspace ? (
         <>
-          {skeleton ? (
-            <WorkspaceSkeleton />
-          ) : (
-            <WorkspacesInHome workspaces={workspaces} />
-          )}
+          <WorkspacesInHome setNoWorkspace={setNoWorkspace}/>
           <AsideBar openModal={setCreateModal} openJoinModal={setJoinModal} />
         </>
       ) : (
