a43ca0b79898f37bc3200f8d2e2be4873c230cdd
ankushh14 <ankushshenoy97@gmail.com>
2024-02-12 14:10:10 +0530

    style: fixed height

--- src/components/home/AsideBar.jsx
+++ src/components/home/AsideBar.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 export default function AsideBar({openModal,openJoinModal}) {
     const {darkMode} = useTheme()
     return (
-        <div className={`aside-bar  lg:static bottom-0  flex flex-col w-full lg:w-[30%] space-y-3 p-4 border-t lg:border-l lg:border-t-0 ${darkMode?"bg-[#212529]  border-white":"bg-white border-[#d3d3d3]"}`}>
+        <div className={`aside-bar  lg:static  flex flex-col w-full lg:w-[30%] space-y-3 p-4 border-t lg:border-l lg:border-t-0 ${darkMode?"bg-[#212529]  border-white":"bg-white border-[#d3d3d3]"}`}>
             <button type="button" className={`${darkMode?"bg-white text-black":"bg-black text-white"} border border-black rounded-md px-8 py-3 flex justify-center items-center active:scale-95 transition-all duration-500`} onClick={()=>openModal(true)}>Create a new workspace</button>
             <button type="button" className={`${darkMode?"bg-white text-black":"bg-black text-white"} border border-black rounded-md px-8 py-3 flex justify-center items-center active:scale-95 transition-all duration-500`} onClick={()=>openJoinModal(true)}>Join a workspace</button>
         </div>
