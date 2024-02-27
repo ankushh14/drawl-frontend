334d7e6f5d34a0c883f5fd97568c2d98f134c6b9
ankushh14 <ankushshenoy97@gmail.com>
2024-02-28 00:36:25 +0530

    fix : Dark mode color fixes

--- src/utils/skeletons/SkeletonCard.jsx
+++ src/utils/skeletons/SkeletonCard.jsx
@@ -1,6 +1,14 @@
+import useTheme from "../../hooks/useTheme";
+
 export default function SkeletonCard() {
+  const { darkMode } = useTheme()
   return (
-    <div className={`card w-[300px]  h-[330px] p-2 m-4 px-4 rounded bg-white text-black flex flex-col justify-between shadow shadow-inherit cursor-pointer hover:shadow-md`}>
+    <div className={`card w-[300px]  h-[330px] p-2 m-4 px-4 rounded flex flex-col justify-between shadow shadow-inherit cursor-pointer hover:shadow-md
+    ${
+      darkMode
+        ? "bg-[#393f45] text-white shadow-slate-800"
+        : "bg-white text-black shadow-slate-500"
+    }`}>
       <div className="name-skeleton w-[90%] h-[30px] rounded-md bg-slate-400 animate-pulse"></div>
       <div className="group-skeleton-outer w-full h-[250px] my-2 flex justify-center items-center flex-col">
         <div className="group-icon-skelton w-[100px] h-[100px] bg-slate-400 animate-pulse rounded-xl ">
