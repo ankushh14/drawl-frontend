6cf1e017e562c82e9e79914d5742a0951b88c465
ankushh14 <ankushshenoy97@gmail.com>
2024-03-08 16:42:35 +0530

    fix : whiteboard darkmode state

--- src/components/whiteboard/MainWhiteBoard.jsx
+++ src/components/whiteboard/MainWhiteBoard.jsx
@@ -5,6 +5,7 @@ import { useWorkspace } from "../../hooks/useWorkspace";
 import useBoardStore from "../../hooks/useBoardStore";
 import { useAuth } from "../../hooks/useAuth";
 import { myOverrides } from "../../utils/tldraw/new_Overrides";
+import { useEffect, useState } from "react";
 
 
 
@@ -13,10 +14,18 @@ export default function MainWhiteBoard() {
   const {ID} = useWorkspace()
   const {user} = useAuth()
   const store = useBoardStore({roomID:ID,userName:user.email})
+  const [editor,setEditor] = useState(null)
+
+  useEffect(()=>{
+    if(editor !==null || editor !== undefined){
+      editor.user.updateUserPreferences({isDarkMode : darkMode})
+      console.log(editor)
+    }
+  },[darkMode,editor])
 
   return (
     <div className='w-full fixed md:w-[65%] xl:w-[75%] md:static h-[calc(100%-41.6px)] md:h-full'>
-      <Tldraw inferDarkMode = {darkMode} store={store} overrides={myOverrides} className={`border ${darkMode?"border-[#30363b]":"border-[#d3d3d3]"}`} />
+      <Tldraw store={store} overrides={myOverrides} className={`border ${darkMode?"border-[#30363b]":"border-[#d3d3d3]"}`} onMount={(editor)=>setEditor(editor)}/>
     </div>
   )
 }
