c816decab1434e25e983c6d18dd85c94aaaa7b32
ankushh14 <ankushshenoy97@gmail.com>
2024-03-11 21:03:37 +0530

    style : z-index

--- src/utils/loaders/MainLoader.jsx
+++ src/utils/loaders/MainLoader.jsx
@@ -4,7 +4,7 @@ export default function MainLoader() {
   const { darkMode } = useTheme();
   return (
       <div
-        className={`back-div fixed top-0 left-0 right-0 bottom-0 z-40 flex justify-center items-center 
+        className={`back-div fixed top-0 left-0 right-0 bottom-0 z-[1000] flex justify-center items-center 
         ${darkMode ? "bg-[#212529] text-white" : "bg-white text-black"}`}
       >
         <div className="load-div flex justify-center space-x-1 items-center">
